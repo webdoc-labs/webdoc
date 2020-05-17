8e7ca375cc72104499127473c223f35e00cb1068
Shukant Pal <shukantpal@outlook.com>
2020-05-17 13:36:17 -0400

    Fix parseParams

--- packages/webdoc-parser/src/tag-parsers/parseParam.js
+++ packages/webdoc-parser/src/tag-parsers/parseParam.js
@@ -12,7 +12,7 @@ function extractIdentifer(from: string, index: number = 0): {
   closureEnd: number
   } {
   // Extracts the [ident=defaultValue] group. The brackets & equal sign need not exist.
-  const identClosureMatch = /((\[)([^\]])+(\]))|(([^\s])([\[])?([^\s])+)?/.exec(from);
+  const identClosureMatch = /((\[)([^\]])+(\]))|(\S)+/.exec(from);
 
   if (!identClosureMatch) {
     return {
