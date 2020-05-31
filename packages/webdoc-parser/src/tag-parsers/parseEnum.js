8e8e6e3786acc99105565bfc5d206a54eaffac31
Shukant Pal <shukantpal@outlook.com>
2020-05-31 13:52:37 -0400

    parseEnum; test for parseProperty

---
+++ packages/webdoc-parser/src/tag-parsers/parseEnum.js
@@ -0,0 +1,24 @@
+// @flow
+import type {Doc, EnumTag} from "@webdoc/types";
+import {matchDataTypeClosure, StringUtils} from "./helper";
+
+export function parseEnum(value: string, doc: $Shape<Doc>): EnumTag {
+  const dataTypeClosure = matchDataTypeClosure(value);
+
+  if (dataTypeClosure) {
+    value = StringUtils.del(value, dataTypeClosure).trimStart();
+
+    const dataType = dataTypeClosure[0];
+
+    doc.dataType = [dataType, dataType];
+  }
+
+  if (value) {
+    doc.name = value.trim();
+  }
+
+  return {
+    value,
+    type: "EnumDoc",
+  };
+}
---
+++ packages/webdoc-parser/test/tag-parsers/parseProperty.js
@@ -0,0 +1,76 @@
+const {parseProperty} = require("../../lib/tag-parsers/parseProperty");
+const expect = require("chai").expect;
+
+describe("@webdoc/parser.parseProperty", function() {
+  it("@property {DataType} propertyName <description>", function() {
+    const doc = {};
+
+    parseProperty("{DataType} propertyName <description>", doc);
+
+    const propertyDoc = doc.members[0];
+
+    expect(propertyDoc.name).to.equal("propertyName");
+    expect(propertyDoc.dataType).to.not.equal(undefined);
+    expect(propertyDoc.dataType[0]).to.equal("DataType");
+    expect(propertyDoc.description).to.equal("<description>");
+  });
+
+  it("@property {DataType} propertyName=dataValue <description>", function() {
+    const doc = {};
+
+    parseProperty("{DataType} propertyName=dataValue <description>", doc);
+
+    const propertyDoc = doc.members[0];
+
+    expect(propertyDoc.name).to.equal("propertyName");
+    expect(propertyDoc.dataType).to.not.equal(undefined);
+    expect(propertyDoc.dataType[0]).to.equal("DataType");
+    expect(propertyDoc.constant).to.equal(true);
+    expect(propertyDoc.dataValue).to.equal("dataValue");
+    expect(propertyDoc.description).to.equal("<description>");
+  });
+
+  it("@property {DataType} propertyName=\"DATA VALUE\" <description>", function() {
+    const doc = {};
+
+    parseProperty("{DataType} propertyName=\"DATA VALUE\" <description>", doc);
+
+    const propertyDoc = doc.members[0];
+
+    expect(propertyDoc.name).to.equal("propertyName");
+    expect(propertyDoc.dataType).to.not.equal(undefined);
+    expect(propertyDoc.dataType[0]).to.equal("DataType");
+    expect(propertyDoc.constant).to.equal(true);
+    expect(propertyDoc.dataValue).to.equal("\"DATA VALUE\"");
+    expect(propertyDoc.description).to.equal("<description>");
+  });
+
+  it("@property {DataType}[propertyName] <description>", function() {
+    const doc = {};
+
+    parseProperty("{DataType}[propertyName] <description>", doc);
+
+    const propertyDoc = doc.members[0];
+
+    expect(propertyDoc.name).to.equal("propertyName");
+    expect(propertyDoc.dataType).to.not.equal(undefined);
+    expect(propertyDoc.dataType[0]).to.equal("DataType");
+    expect(propertyDoc.optional).to.equal(true);
+    expect(propertyDoc.description).to.equal("<description>");
+  });
+
+  it("@property {DataType}[propertyName=defaultValue] <description>", function() {
+    const doc = {};
+
+    parseProperty("{DataType}[propertyName=defaultValue] <description>", doc);
+
+    const propertyDoc = doc.members[0];
+
+    expect(propertyDoc.name).to.equal("propertyName");
+    expect(propertyDoc.dataType).to.not.equal(undefined);
+    expect(propertyDoc.dataType[0]).to.equal("DataType");
+    expect(propertyDoc.optional).to.equal(true);
+    expect(propertyDoc.defaultValue).to.equal("defaultValue");
+    expect(propertyDoc.description).to.equal("<description>");
+  });
+});
