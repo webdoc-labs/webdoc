0e1b9c8660abe6c56e8038a1b1c4a03f33b5a13b
Shukant Pal <shukantpal@outlook.com>
2020-11-05 12:35:56 -0500

    Fix linting problems (pass 3)

--- packages/webdoc-parser/src/tag-parsers/parseScope.js
+++ packages/webdoc-parser/src/tag-parsers/parseScope.js
@@ -1,4 +1,5 @@
-import type {InnerTag, InstanceTag, StaticTag} from "@webdoc/types";
+import type {AccessTag, InnerTag, InstanceTag, StaticTag} from "@webdoc/types";
+import {parserLogger, tag} from "../Logger";
 
 // @scope <"static" | "instance" | "inner">
 // @static
