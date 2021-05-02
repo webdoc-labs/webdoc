64603a749a8dd343cba8b67fed5e558dd16f6659
Shukant Pal <sukantk2002@outlook.com>
2021-05-02 14:17:20 -0400

    @webdoc/externalize DocumentedInterface

---
+++ packages/webdoc-externalize/src/read.js
@@ -0,0 +1,60 @@
+// @flow
+
+import * as model from "@webdoc/model";
+import {BASE_PROPS} from "./const";
+import type {Doc} from "@webdoc/types";
+import type {DocumentedInterface} from "./types";
+import _ from "lodash";
+
+function deserializeTree(serialized: any, parent: ?Doc): Doc {
+  if (!parent && serialized.type !== "RootDoc") {
+    throw new Error("Tree root must declare type RootDoc!");
+  }
+
+  const doc: $Shape<Doc> = _.pick(serialized, BASE_PROPS);
+
+  doc.stack = parent ? [...parent.stack, doc.name] : [doc.name];
+
+  if (serialized.description) {
+    doc.description = serialized.description;
+  }
+  if (serialized.members) {
+    doc.members = new Array<Doc>(serialized.members.length);
+
+    for (let i = 0; i < serialized.members.length; i++) {
+      doc.members[i] = deserializeTree(serialized.members[i], (doc: any));
+    }
+  }
+
+  if (parent) {
+    model.addChildDoc(doc, parent);
+  }
+
+  return (doc: any);
+}
+
+/**
+ * Reads the serialized documented interface from a JSON string.
+ *
+ * @param {string} data
+ * @return {RootDoc}
+ */
+export default function read(data: string): DocumentedInterface {
+  const documentedInterface = (JSON.parse(data): any);
+
+  if (!("version" in documentedInterface)) {
+    return ({
+      version: "0.0.0",
+      metadata: {},
+      root: (deserializeTree(documentedInterface.root, null): any),
+      registry: {},
+    }: DocumentedInterface);
+  }
+
+  return {
+    version: documentedInterface.version,
+    metadata: documentedInterface.metadata,
+    root: (deserializeTree(documentedInterface.root, null): any),
+    registry: {},
+  };
+}
