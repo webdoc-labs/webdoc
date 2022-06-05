c1b3fb59c59b730d5828e1408a2eb250e69e800a
Shukant Pal <SukantK2002@outlook.com>
2022-06-05 17:15:41 -0400

    Feature: Support @classdesc for ES6 classes

--- packages/webdoc-parser/src/transformer/mod-es5-constructor.js
+++ packages/webdoc-parser/src/transformer/mod-es5-constructor.js
@@ -9,21 +9,26 @@ import type {Doc} from "@webdoc/types";
  * @param {Doc} doc
  */
 export default function es5Constructor(doc: Doc) {
-  if (doc.type === "ClassDoc" && !doc.members.find(isConstructor)) {
+  if (doc.type === "ClassDoc") {
     const classDescTag = doc.tags && doc.tags.find((tag) => tag.type === "ClassDescTag");
-    const constructorDoc = createDoc("constructor", "MethodDoc", {
-      brief: doc.brief,
-      description: classDescTag ? doc.description : "",
-      params: doc.params ?? [],
-      returns: doc.returns,
-    });
 
-    if (classDescTag) {
-      delete doc.brief;
-      doc.description = classDescTag.value;
+    if (!doc.members.find(isConstructor)) {
+      const constructorDoc = createDoc("constructor", "MethodDoc", {
+        brief: doc.brief,
+        description: classDescTag ? doc.description : "",
+        params: doc.params ?? [],
+        returns: doc.returns,
+      });
+
+      if (classDescTag) {
+        delete doc.brief;
+        doc.description = classDescTag.value;
+      }
+
+      addChildDoc(constructorDoc, doc);
+    } else if (classDescTag) {
+      doc.description += classDescTag.value;
     }
-
-    addChildDoc(constructorDoc, doc);
   }
 
   // Continue down because classes still may have classes as members.
