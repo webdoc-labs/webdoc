30050f562d5e327efb55d30a879c0f7b449a86e3
Shukant Pal <SukantK2002@outlook.com>
2022-06-05 20:00:51 -0400

    Feature: Object pattern parameter parsing

--- packages/webdoc-parser/test/lang-ts.js
+++ packages/webdoc-parser/test/lang-ts.js
@@ -238,4 +238,34 @@ describe("@webdoc/parser.LanguageIntegration{@lang ts}", function() {
 
     expect(symbolTree.members[0].meta.params[0].dataType[0]).to.equal("string");
   });
+
+  it("should expand object pattern parameters", function() {
+    const symbolTree = buildSymbolTree(`
+      function length({ p0: { x: x0, y: y0 } }: { p0: { x: number, y: number} }) {}
+    `, "*.ts");
+
+    const lengthFn = symbolTree.members[0];
+
+    expect(lengthFn.meta.params.length).to.equal(3);
+    expect(lengthFn.meta.params[1].identifier).to.equal(".p0.x");
+    expect(lengthFn.meta.params[1].dataType[0]).to.equal("number");
+    expect(lengthFn.meta.params[2].identifier).to.equal(".p0.y");
+    expect(lengthFn.meta.params[2].dataType[0]).to.equal("number");
+  });
+
+  it("should expand object pattern parameters with default values", function() {
+    const symbolTree = buildSymbolTree(`
+      function length({ p0: { x: x0, y: y0 } }: { p0: { x: number, y: number} }
+        = { p0: { x: 1, y: 2 } } ) {}
+    `, "*.ts");
+
+    const lengthFn = symbolTree.members[0];
+
+    expect(lengthFn.meta.params.length).to.equal(3);
+    expect(lengthFn.meta.params[0].dataType[0]).to.equal("{ p0 : { x : number, y : number } }");
+    expect(lengthFn.meta.params[1].default).to.equal("1");
+    expect(lengthFn.meta.params[2].default).to.equal("2");
+    expect(lengthFn.meta.params[1].dataType[0]).to.equal("number");
+    expect(lengthFn.meta.params[2].dataType[0]).to.equal("number");
+  });
 });
